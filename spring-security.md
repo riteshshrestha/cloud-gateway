cbc2d4d55a2c7d8dbac8016b5c43ba171bb280c5
riteshshrestha <ritesh.shrestha@gmail.com>
2021-01-10 07:12:49 -0500

    cloud arch

---
+++ spring-security.md
@@ -0,0 +1,4 @@
+## Layerd Security Services
+Spring Security allows you to secure your application at
+different levels, and to secure your web URLs, views, service methods, and domain model. You
+can pick and combine these features to achieve your security goals.
--- src/main/java/com/rs/gateway/config/RouteConfig.java
+++ src/main/java/com/rs/gateway/config/RouteConfig.java
@@ -10,29 +10,29 @@ public class RouteConfig {
 
     /**
      * Java based routing with builder
-     * @param builder
-     * @return
-     * route to http://httpbin.org/get
      *
+     * @param builder
+     * @return route to http://httpbin.org/get
+     * <p>
      * curl --dump-header - --header 'Host: www.hystrix.com' http://localhost:8080/delay/3
      */
 
     @Bean
     public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+
         return builder.routes().route(p -> p.path("/get-test")
-                .filters(f->f.addRequestHeader("hello","world"))
+                .filters(f -> f.addRequestHeader("hello", "world"))
                 .uri("http://httpbin.org:80"))
                 //Hystrix circuit breaker
                 .route(p -> p
                         .host("*.hystrix.com")
-                        .filters(f-> f.hystrix(config -> config
+                        .filters(f -> f.hystrix(config -> config
                                 .setName("myCmd")
                                 // uri=fallback
                                 .setFallbackUri("forward:/fallback")))
                         .uri("http://httpbin.org:80"))
-                        .build();
+                .build();
     }
-
 
 
 }
